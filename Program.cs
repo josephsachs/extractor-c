379e184e80f09f0e43a41b0720d41c0eb345f28f
Joseph Sachs <jhalesachs@gmail.com>
2025-04-19 14:34:46 -0400

    Improve verify prompt further with more checks

--- Program.cs
+++ Program.cs
@@ -11,7 +11,6 @@ builder.Services.Configure<OpenAIConfigData>(
 
 builder.Services.AddControllers();
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddHttpClient();
